1e9ca22f1921e3765c5a1f2745d7f69d9d91b1af
Ericcs10 <eric.camargo.silva@gmail.com>
2023-05-16 00:12:37 -0300

    #79 feat: requisições movimentações
    Co-authored-by:suzaneduarte<ssuzane9@hotmail.com

--- src/config/routes/Routes.tsx
+++ src/config/routes/Routes.tsx
@@ -11,7 +11,8 @@ import { Routes, Route } from 'react-router-dom';
 import { Login } from '@/pages/login';
 import { DefaultLayout } from '@/components/layout/default-layout';
 import { EquipRegister } from '@/pages/equipment-register';
-import { EquipmentTable }from '@/pages/equipaments/EquipamentsControl';
+import { EquipmentTable } from '@/pages/equipaments/EquipamentsControl';
+import { MovementsControl } from '@/pages/movements/MovementControl';
 
 export function Router() {
   return (
@@ -40,6 +41,7 @@ export function Router() {
       {/* ROTAS PUBLICAS */}
       <Route path="/login" element={<Login />} />
       <Route path="/equipaments" element={<EquipmentTable />} />
+      <Route path="/movements" element={<MovementsControl />} />
       <Route path="*" element={<p>404</p>} />
     </Routes>
   );
---
+++ src/pages/movements/MovementControl.tsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react';
+import { ArrowRightIcon, ArrowLeftIcon } from '@chakra-ui/icons';
+import { MdBuild, MdCall } from 'react-icons/md';
+import {
+  Select,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Input,
+  Button,
+  Center,
+  Divider,
+  Box,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { SideBar } from '@/components/side-bar';
+
+function Movimentacoes() {
+  const mockData = [
+    {
+      Id: 13276989755488998,
+      tipo: 'Emprestimo',
+      destino: 'Divisão de Suporte Técnico',
+      data: '01/05/2023',
+      quantidade: 2,
+    },
+    {
+      id: 7798791893792675,
+      tipo: 'Responsabilidade',
+      destino: 'Escola Superior de Polícia Civil',
+      data: '03/05/2023',
+      quantidade: 1,
+    },
+    {
+      id: 34367686545435764,
+      tipo: 'Emprestimo',
+      destino: 'Escola Superior de Polícia Civil',
+      data: '05/05/2023',
+      quantidade: 3,
+    },
+  ];
+
+  return (
+    <Box paddingY="10" paddingX="300">
+      <Box mb="10px">
+        <Text color="#000000" fontWeight="semibold" fontSize="4xl">
+          Movimentações
+        </Text>
+        <Text color="#000000" fontWeight="medium" fontSize="2xl">
+          Últimas Movimentações
+        </Text>
+      </Box>
+      <Box paddingLeft="300">
+        <Divider borderColor="#000000" />
+      </Box>
+      <Box p={4}>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>ID Movimentação</Th>
+              <Th>Tipo</Th>
+              <Th>Destino</Th>
+              <Th>Data</Th>
+              <Th>Quantidade</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {mockData.map((data) => (
+              <Tr key={data.id}>
+                <Td>{data.id}</Td>
+                <Td>{data.tipo}</Td>
+                <Td>{data.destino}</Td>
+                <Td>{data.data}</Td>
+                <Td>{data.quantidade}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+}
+
+export default Movimentacoes;
