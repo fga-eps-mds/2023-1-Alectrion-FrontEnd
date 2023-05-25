1a32c419025dd74f8f64b700760646ef495c7295
Suzane Duarte <ssuzane9@hotmail.com>
2023-05-24 23:05:50 -0300

    #88 feat: refatorando testes de equipment

--- src/pages/equipments/equipaments.spec.tsx
+++ src/pages/equipments/equipaments.spec.tsx
@@ -3,66 +3,41 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { EquipmentTable } from '@/pages/equipments/EquipamentsControl';
 
+const renderComponent = () =>
+  render(
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<EquipmentTable />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+
 describe('Equipments', () => {
   it('should display a list', async () => {
-    const { findByRole } = render(
-      <ChakraProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<EquipmentTable />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    );
+    const { findByRole } = renderComponent();
 
     const list = await findByRole('table');
     expect(list).toBeInTheDocument();
   });
 
   it('should display a button to create equipments', async () => {
-    const { queryByText } = render(
-      <ChakraProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<EquipmentTable />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    );
+    const { queryByText } = renderComponent();
 
     const button = await queryByText('Cadastrar Equipamento');
-    if (button) {
-      expect(button).toBeInTheDocument();
-    }
+    expect(button).toBeInTheDocument();
   });
 
   it('should display a button to move equipments', async () => {
-    const { queryByText } = render(
-      <ChakraProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<EquipmentTable />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    );
+    const { queryByText } = renderComponent();
 
     const button = await queryByText('Movimentar');
-    if (button) {
-      expect(button).toBeInTheDocument();
-    }
+    expect(button).toBeInTheDocument();
   });
 
   it('should display a search bar', async () => {
-    const { findByPlaceholderText } = render(
-      <ChakraProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<EquipmentTable />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    );
+    const { findByPlaceholderText } = renderComponent();
 
     const bar = await findByPlaceholderText('Pesquisa');
     expect(bar).toBeInTheDocument();
