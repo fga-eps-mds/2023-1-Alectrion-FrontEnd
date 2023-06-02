f6f82fc78d6d13b66fb7f6ceb33a4e1270f3ccdb
luana <lulyluana53@hotmail.com>
2023-06-02 11:57:50 -0300

    #103 feat: estruturação dos filtros de ordem de serviço
    Co-authored-by: esteerlino <esteerlino@gmail.com>

--- src/pages/order-service/OrderServiceControl.tsx
+++ src/pages/order-service/OrderServiceControl.tsx
@@ -24,8 +24,11 @@ import { SideBar } from '@/components/side-bar';
 import { api } from '../../config/lib/axios';
 import { theme } from '@/styles/theme';
 import { ControlledSelect } from '@/components/form-fields/controlled-select/index';
-import { TIPOS_EQUIPAMENTO } from '@/constants/equipment';
-import { OSStatusMap, OSStatusStyleMap } from '@/constants/orderservice';
+import { STATUS, TIPOS_EQUIPAMENTO, Workstation } from '@/constants/equipment';
+import { Datepicker } from '@/components/form-fields/date';
+import { Input } from '@/components/form-fields/input';
+import { EquipmentRegisterModal } from '@/components/equipment-register-modal';
+import { OSStatusMap } from '@/constants/orderservice';
 
 interface ISelectOption {
   label: string;
@@ -76,65 +79,90 @@ type FilterValues = {
 };
 
 function OrderServiceTable() {
-  const [orderServices, setOrderServices] = useState<OrderServiceData[]>([]);
-  const [nextOrderServices, setNextOrderServices] = useState<
-    OrderServiceData[]
-  >([]);
+    const [orderServices, setOrderServices] = useState<OrderServiceData[]>([]);
+    const [nextOrderServices, setNextOrderServices] = useState<OrderServiceData[]>([]);
+  
+    const [selectedOrderServiceToEdit, setSelectedOrderServiceToEdit] =
+    useState<OrderServiceData>();
+    const [refreshRequest, setRefreshRequest] = useState<boolean>(false);
+    // const [workstations, setWorkstations] = useState<ISelectOption[]>();
 
-  const [refreshRequest, setRefreshRequest] = useState<boolean>(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [offset, setOffset] = useState(0);
+    const limit = 10;
+    const [filter, setFilter] = useState<string>('');
+    const [search, setSearch] = useState<string>('');
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [offset, setOffset] = useState(0);
-  const limit = 10;
-  const [filter, setFilter] = useState<string>('');
-  const [search, setSearch] = useState<string>('');
+    const {
+      control,
+      watch,
+      register,
+      formState: { errors },
+    } = useForm<FilterValues>({ mode: 'onChange' });
+  
+    const watchFilter = watch();
+  
+    const handleFilterChange = () => {
+      const { type, DateOS, situation } = watchFilter;
+  
+      let formattedDate;
+      if (
+        DateOS !== null &&
+        DateOS !== '' &&
+        DateOS
+      ) {
+        formattedDate = new Date(DateOS).toLocaleDateString('en-us');
+        console.log(formattedDate);
+      }
 
-  const {
-    control,
-    watch,
-    formState: { errors },
-  } = useForm<FilterValues>({ mode: 'onChange' });
-
-  const watchFilter = watch();
-
-  const handleFilterChange = () => {
-    const { type, DateOS, situation } = watchFilter;
-
-    let formattedDate;
-    if (DateOS !== null && DateOS !== '' && DateOS) {
-      formattedDate = new Date(DateOS).toLocaleDateString('en-us');
-    }
-
-    const dataFormatted = {
-      type: type?.value,
-      updatedAt: formattedDate,
-      situation: situation?.value,
-      // unit: unit?.value,
-      search,
-    };
-
-    const filteredDataFormatted = [
-      ...Object.entries(dataFormatted).filter(
-        (field) => field[1] !== undefined && field[1] !== ''
-      ),
-    ];
-
-    const query = `${filteredDataFormatted
-      .map((field) => `${field[0]}=${field[1]}`)
-      .join('&')}`;
-    setFilter(query);
+      const dataFormatted = {
+        type: type?.value,
+        updatedAt: formattedDate,
+        situation: situation?.value,
+        // unit: unit?.value,
+        search,
+      };
+  
+      const filteredDataFormatted = [
+        ...Object.entries(dataFormatted).filter(
+          (field) => field[1] !== undefined && field[1] !== ''
+        ),
+      ];
+  
+      const query = `${filteredDataFormatted
+        .map((field) => `${field[0]}=${field[1]}`)
+        .join('&')}`;
+      setFilter(query);
   };
 
-  const debounce = <T extends (...args: any[]) => void>(fn: T, ms = 400) => {
-    let timeoutId: ReturnType<typeof setTimeout>;
-    return function (this: any, ...args: Parameters<T>) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => fn.apply(this, args), ms);
+
+    /*const formattedWorkstations = (data: Workstation[]): ISelectOption[] => {
+      return data?.map((item: Workstation) => {
+        return { label: item.name, value: item.name };
+      });
+    };*/
+
+    /* const getWorkstations = async () => {
+      apiSchedula
+        .get<Workstation[]>('workstations')
+        .then((response) => {
+        setWorkstations(formattedWorkstations(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };*/
+
+    const debounce = <T extends (...args: any[]) => void>(fn: T, ms = 400) => {
+      let timeoutId: ReturnType<typeof setTimeout>;
+      return function (this: any, ...args: Parameters<T>) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn.apply(this, args), ms);
+      };
     };
-  };
-  const handleSearch = debounce(() => {
-    setSearch(watchFilter.search);
-  }, 400);
+    const handleSearch = debounce(() => {
+      setSearch(watchFilter.search);
+    }, 400);
 
   const fetchItems = async () => {
     try {
@@ -268,6 +296,41 @@ function OrderServiceTable() {
                     },
                   }}
                 >
+                  
+                  <form id="equipment-filter" style={{ width: '100%' }}>
+                    <Flex gap="5px" alignItems="5px" mb="15px">
+                      <ControlledSelect
+                        control={control}
+                        name="type"
+                        id="type"
+                        options={TIPOS_EQUIPAMENTO}
+                        placeholder="Tipo"
+                        cursor="pointer"
+                        variant="unstyled"
+                        fontWeight="semibold"
+                        size="sm"
+                      />
+                      
+                      <Select
+                        placeholder="Localização"
+                        cursor="pointer"
+                        variant="unstyled"
+                        fontWeight="semibold"
+                        size="sm"
+                      >
+                        <option value="option1">Goiania</option>
+                      </Select>
+                      <Select
+                        placeholder="Situação"
+                        cursor="pointer"
+                        variant="unstyled"
+                        fontWeight="semibold"
+                        size="sm"
+                      >
+                        <option value="option1">Ativo</option>
+                      </Select>
+                    </Flex>
+                  </form>
                   <Table variant="striped" colorScheme="orange" width="100%">
                     <Thead
                       bg={theme.colors.primary}
