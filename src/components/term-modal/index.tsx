1073ab3834aaa9d427c52ccf960b36c1e7f13dde
PabloGJBS <plabo.jbs@gmail.com>
2023-06-06 14:35:46 -0300

    #119 fix: correção no estilo do modal de termo
    Co-authored-by: DafneM <moretti.dafne2@gmail.com>

--- src/components/term-modal/index.tsx
+++ src/components/term-modal/index.tsx
@@ -1,23 +1,16 @@
-
-import {
-  Flex,
-  Text,
-  Button,
-  useDisclosure,
-} from '@chakra-ui/react';
+import { Flex, Text, Button, useDisclosure } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { Modal } from '../modal';
 import { MovementsPDF } from '../movements-pdf/MovementsPdfDocument';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import { MovimentacaoTipoMap } from '@/constants/movements';
 import { movement } from '@/pages/movements/MovementControl';
+import { MdDescription } from 'react-icons/md';
 
-
-    
 type TermModalProps = {
   isOpen: boolean;
   onClose(): void;
-  selectedMoviment: movement | undefined ;
+  selectedMoviment: movement | undefined;
   refreshRequest: boolean;
   setRefreshRequest: React.Dispatch<React.SetStateAction<boolean>>;
 };
@@ -28,10 +21,10 @@ export function TermModal({
   selectedMoviment,
   refreshRequest,
   setRefreshRequest,
-
 }: TermModalProps) {
-  const { isOpen: displayTextModalOpen, onOpen: handleOpenTextModal, onClose: handleCloseTextModal } = useDisclosure();
-  console.log(selectedMoviment)
+  const { isOpen: displayTextModalOpen, onOpen: handleOpenTextModal, onClose: handleCloseTextModal } =
+    useDisclosure();
+  console.log(selectedMoviment);
   const onCloseCallback = () => {
     onClose();
   };
@@ -39,11 +32,15 @@ export function TermModal({
   return (
     <>
       <Modal
-        title={`Termo de ${MovimentacaoTipoMap.get(parseInt(String(selectedMoviment?.type)) || 0) as string}`}
+        title= {`Termo de ${MovimentacaoTipoMap.get(parseInt(String(selectedMoviment?.type)) || 0) as string}`}
         isOpen={isOpen}
         onClose={onCloseCallback}
         size="2xl"
       >
+        <Text textAlign="center" mt="1rem" style={{ display: 'flex', alignItems: 'center' }}fontWeight="bold" fontSize="larger">
+          <MdDescription size={40} />
+          termo_de_{MovimentacaoTipoMap.get(parseInt(String(selectedMoviment?.type)) || 0) as string}.pdf
+        </Text>
         <Flex gap="4rem" mt="2rem" mb="1rem" justify="center">
           <Button variant="secondary" onClick={onCloseCallback}>
             Cancelar
@@ -60,7 +57,7 @@ export function TermModal({
             fileName={`termo_de_${MovimentacaoTipoMap.get(parseInt(String(selectedMoviment?.type)) || 0) as string}_010101`}
           >
             {({ loading }) => (
-              <Button isLoading={loading} variant="primary" onClick={()=>{setRefreshRequest(!refreshRequest);onClose()}}>
+              <Button isLoading={loading} variant="primary" onClick={() => { setRefreshRequest(!refreshRequest); onClose(); }}>
                 Imprimir
               </Button>
             )}
