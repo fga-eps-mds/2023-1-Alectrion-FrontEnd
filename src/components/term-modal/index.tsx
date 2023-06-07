dd79640ba94db53663b842bb850e97c0435c951d
Leofbrgs <leonardo81733@icloud.com>
2023-06-07 09:22:38 -0300

    #56 Condicional baixa

--- src/components/term-modal/index.tsx
+++ src/components/term-modal/index.tsx
@@ -1,25 +1,15 @@
-import { useForm } from 'react-hook-form';
-import {
-  Flex,
-  Text,
-  Grid,
-  GridItem,
-  Button,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Td,
-  Checkbox,
-  TableContainer,
-} from '@chakra-ui/react';
-import { useState } from 'react';
+import { Flex, Text, Button } from '@chakra-ui/react';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import { MdDescription } from 'react-icons/md';
 import { Modal } from '../modal';
-import movimentacaoPDF from '../pdf/pdf';
+import { MovementsPDF } from '../movements-pdf/MovementsPdfDocument';
+import { MovimentacaoTipoMap } from '@/constants/movements';
+import { movement } from '@/pages/movements/MovementControl';
 
 type TermModalProps = {
   isOpen: boolean;
   onClose(): void;
+  selectedMoviment: movement | undefined;
   refreshRequest: boolean;
   setRefreshRequest: React.Dispatch<React.SetStateAction<boolean>>;
 };
@@ -27,6 +17,7 @@ type TermModalProps = {
 export function TermModal({
   isOpen,
   onClose,
+  selectedMoviment,
   refreshRequest,
   setRefreshRequest,
 }: TermModalProps) {
@@ -34,46 +25,68 @@ export function TermModal({
     onClose();
   };
 
-  const [displayTextModalOpen, setDisplayTextModalOpen] = useState(false);
-
-  const handleOpenTextModal = () => {
-    setDisplayTextModalOpen(true);
-  };
-
-  const handleCloseTextModal = () => {
-    setDisplayTextModalOpen(false);
-  };
-
   return (
-    <>
-      <Button onClick={handleOpenTextModal}>Abrir Texto</Button>
-
-      <Modal
-        title="Texto Gerado"
-        isOpen={displayTextModalOpen}
-        onClose={handleCloseTextModal}
-        size="md"
+    <Modal
+      title={`Termo de ${MovimentacaoTipoMap.get(
+        parseInt(String(selectedMoviment?.type), 10) ?? 0
+      ) as string
+        }`}
+      isOpen={isOpen}
+      onClose={onCloseCallback}
+      size="2xl"
+    >
+      <Text
+        textAlign="center"
+        mt="1rem"
+        style={{ display: 'flex', alignItems: 'center' }}
+        fontWeight="bold"
+        fontSize="larger"
       >
-        {/* depois ver a logica de como pegar o tipo de termo pra printar aqui*/}
-        <Text textAlign="center">Texto Gerado</Text>
-      </Modal>
-
-      <Modal
-        title="Termo de Responsabilidade"
-        isOpen={isOpen}
-        onClose={onCloseCallback}
-        size="2xl"
-      >
-        <Flex gap="4rem" mt="2rem" mb="1rem" justify="center">
-          <Button variant="secondary" onClick={onCloseCallback}>
-            Fechar
-          </Button>
-          <Button type="submit" variant="primary" onClick={movimentacaoPDF}>
-            Imprimir
-          </Button>
-        </Flex>
-
-      </Modal>
-    </>
+        <MdDescription size={40} />
+        termo_de_
+        {
+          MovimentacaoTipoMap.get(
+            parseInt(String(selectedMoviment?.type), 10) ?? 0
+          ) as string
+        }
+        .pdf
+      </Text>
+      <Flex gap="4rem" mt="2rem" mb="1rem" justify="center">
+        <Button variant="secondary" onClick={onCloseCallback}>
+          Cancelar
+        </Button>
+        <PDFDownloadLink
+          document={
+            <MovementsPDF
+              title={
+                MovimentacaoTipoMap.get(
+                  parseInt(String(selectedMoviment?.type), 10) ?? 0
+                ) as string
+              }
+              equipments={selectedMoviment?.equipments ?? []}
+              date={selectedMoviment?.date ?? ''}
+              destination={selectedMoviment?.destination.name ?? ''}
+            />
+          }
+          fileName={`termo_de_${MovimentacaoTipoMap.get(
+            parseInt(String(selectedMoviment?.type), 10) ?? 0
+          ) as string
+            }`}
+        >
+          {({ loading }) => (
+            <Button
+              isLoading={loading}
+              variant="primary"
+              onClick={() => {
+                setRefreshRequest(!refreshRequest);
+                onClose();
+              }}
+            >
+              Imprimir
+            </Button>
+          )}
+        </PDFDownloadLink>
+      </Flex>
+    </Modal>
   );
 }
