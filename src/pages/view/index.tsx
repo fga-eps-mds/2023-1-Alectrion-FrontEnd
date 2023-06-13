ac1ef80092ac109794ed9aee71776d4abe379f11
Leofbrgs <leonardo81733@icloud.com>
2023-06-12 21:21:54 -0300

    127 Fix: Fazendo o request para o back-end Co-authored-by: Izarias <pedroaugustoizarias@gmail.com>

--- src/pages/view/index.tsx
+++ src/pages/view/index.tsx
@@ -12,185 +12,190 @@
     Aqui, fizemos modificações no código original para se adequar ao nosso caso específico de uso.
     Quaisquer erros ou bugs nesta implementação são de nossa responsabilidade.
  */
-    import { useState } from 'react';
-    import { SubmitHandler, useForm } from 'react-hook-form';
-    import { Box, Button, Center, Divider, Flex, Input, Text } from '@chakra-ui/react';
-    import {Modal} from '@/components/modal';
-    // import  Equip  from '../edit-equipment';
-    
-    export function View() {
-      const [isLoading, setIsLoading] = useState(false);
-    
-      const generalTypes = ['Webcam', 'Escaneador'];
-    
-      const equipmentDataType = 'Webcam';
-    
-      switch (equipmentDataType) {
-        case generalTypes.find((item) => item === 'Tipo 1'):
-          break;
-        case generalTypes.find((item) => item === equipmentDataType):
-          break;
-        default:
-          break;
-      }
-    
-      const [statusModal, setStatusModal] = useState<boolean>(false);
-        const toggleModal = () => {
-           setStatusModal(!statusModal);
-        };
-      
-      const {
-        register,
-        handleSubmit,
-        formState: { errors },
-      } = useForm<CredentialUser>();
-    
-      const onSubmit: SubmitHandler<CredentialUser> = async () => {
-        setIsLoading(true);
-        // TODO: get function from authContext
-        // await signIn({ username, password });
-        setIsLoading(false);
-      };
-    
-      return (
-      <Center
-        aria-label="form"
-        bgGradient="linear(288.94deg, #F8B86D 0%, #F49320 90.96%)"
-        h="100vh"
-        color="white"
-      >
-        <form onSubmit={handleSubmit(onSubmit)} aria-label="form">
-          <Box
-            bg="white"
-            borderRadius="10px"
-            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25),  0px 4px 4px rgba(0, 0, 0, 0.25),  0px 1px 1px rgba(0, 0, 0, 0.12),  0px 2px 2px rgba(0, 0, 0, 0.12),  0px 8px 8px rgba(0, 0, 0, 0.12);"
-            color="black"
-            paddingY="20"
-            paddingX="20"
-          >
-            <Text mb="39px" color="#605555" fontWeight="semibold" fontSize="4xl">
-              Usuário
-            </Text>
-            <Flex>
-              <Box flex="1" pr="2">
-                <Text
-                  pl="5px"
-                  pb="8px"
-                  color="#605555"
-                  fontWeight="medium"
-                  fontSize="lg"
-                >
-                  Nome
-                </Text>
-                <Input size="lg" fontSize="lg" placeholder="Nome" readOnly/>
-              </Box>
-              <Box flex="1" pl="2">
-                <Text
-                  pl="5px"
-                  pb="8px"
-                  color="#605555"
-                  fontWeight="medium"
-                  fontSize="lg"
-                >
-                  Sobrenome
-                </Text>
-                <Input size="lg" fontSize="lg" placeholder="Sobrenome" readOnly/>
-              </Box>
-            </Flex>
-            <Box>
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Box, Button, Center, Divider, Flex, Input, Text } from '@chakra-ui/react';
+import { AxiosResponse } from 'axios';
+import { useState, useEffect } from 'react';
+import { User, LoginResponse } from '../../constants/user';
+import { api, apiSchedula } from '@/config/lib/axios';
+import { Modal } from '@/components/modal';
+// import  Equip  from '../edit-equipment';
+
+export function View() {
+
+  const [userData, setUserData] = useState<User | undefined>(undefined);
+
+  const loggedUser = JSON.parse(
+    localStorage.getItem('@App:user') || ''
+  ) as unknown as LoginResponse;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CredentialUser>();
+
+  const getUserData = async () => {
+    try {
+      const token = localStorage.getItem('@App:token') || '';
+      const { data }: AxiosResponse<User[]> = await api.get(`user/get`, {
+        params: { userName: loggedUser?.name },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUserData(data[0]);
+    } catch (error) {
+      setUserData(undefined);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(loggedUser)
+    getUserData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  return (
+    <Center
+      aria-label="form"
+      bgGradient="linear(288.94deg, #F8B86D 0%, #F49320 90.96%)"
+      h="100vh"
+      color="white"
+    >
+      <form onSubmit={handleSubmit(() => { })} aria-label="form">
+        <Box
+          bg="white"
+          borderRadius="10px"
+          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25),  0px 4px 4px rgba(0, 0, 0, 0.25),  0px 1px 1px rgba(0, 0, 0, 0.12),  0px 2px 2px rgba(0, 0, 0, 0.12),  0px 8px 8px rgba(0, 0, 0, 0.12);"
+          color="black"
+          paddingY="20"
+          paddingX="20"
+        >
+          <Text mb="39px" color="#605555" fontWeight="semibold" fontSize="4xl">
+            Usuário
+          </Text>
+          <Flex>
+            <Box flex="1" pr="2">
               <Text
                 pl="5px"
                 pb="8px"
                 color="#605555"
                 fontWeight="medium"
                 fontSize="lg"
-                marginTop="8px"
               >
-                E-mail
+                Nome
               </Text>
-              <Input size="lg" fontSize="lg" placeholder="E-mail da pessoa" readOnly/>
+              <Input size="lg" fontSize="lg" placeholder={` ${userData?.username}`} readOnly />
             </Box>
-            <Divider my="4" borderColor="gray.300" />
-            <Flex>
-              <Box flex="1" pr="2">
-                <Text
-                  pl="5px"
-                  pb="8px"
-                  color="#605555"
-                  fontWeight="medium"
-                  fontSize="lg"
-                >
-                  Senha atual
-                </Text>
-                <Input
-                  size="lg"
-                  fontSize="lg"
-                  placeholder="Senha atual da pessoa"
-                  readOnly
-                />
-              </Box>
-              <Box flex="1" pl="2">
-                <Text
-                  pl="5px"
-                  pb="8px"
-                  color="#605555"
-                  fontWeight="medium"
-                  fontSize="lg"
-                >
-                  Nova senha
-                </Text>
-                <Input
-                  size="lg"
-                  fontSize="lg"
-                  placeholder="Nova senha"
-                  readOnly
-                />
-              </Box>
-              <Box mb="20px" flex="1" pl="2">
-                <Text
-                  pl="5px"
-                  pb="8px"
-                  color="#605555"
-                  fontWeight="medium"
-                  fontSize="lg"
-                >
-                  Confirmar senha
-                </Text>
-                <Input
-                  size="lg"
-                  fontSize="lg"
-                  placeholder="Confirmar senha"
-                  readOnly
-                />
-              </Box>
-            </Flex>
-            <Flex justify="space-between">
-                <Button
-                  marginTop="20px"
-                  mb="120px"
-                  type="submit"
-                  paddingX="24"
-                  width="20px"
-                  color="white"
-                  bg="black"
-                  onClick={() => window.history.back()}
-                >
-                  Voltar
-                </Button>
-              
-              <Button
-                marginTop="30px"
-                mb="120px"
-                type="submit"
-                paddingX="24"
-                width="20px"
+            <Box flex="1" pl="2">
+              <Text
+                pl="5px"
+                pb="8px"
+                color="#605555"
+                fontWeight="medium"
+                fontSize="lg"
               >
-                Salvar
-              </Button>
-            </Flex>
+                Sobrenome
+              </Text>
+              <Input size="lg" fontSize="lg" placeholder="Sobrenome" readOnly />
+            </Box>
+          </Flex>
+          <Box>
+            <Text
+              pl="5px"
+              pb="8px"
+              color="#605555"
+              fontWeight="medium"
+              fontSize="lg"
+              marginTop="8px"
+            >
+              E-mail
+            </Text>
+            <Input size="lg" fontSize="lg" placeholder="E-mail da pessoa" readOnly />
           </Box>
-        </form>
-      </Center>
-      );
-    }
-    
+          <Divider my="4" borderColor="gray.300" />
+          <Flex>
+            <Box flex="1" pr="2">
+              <Text
+                pl="5px"
+                pb="8px"
+                color="#605555"
+                fontWeight="medium"
+                fontSize="lg"
+              >
+                Senha atual
+              </Text>
+              <Input
+                size="lg"
+                fontSize="lg"
+                placeholder="Senha atual da pessoa"
+                readOnly
+              />
+            </Box>
+            <Box flex="1" pl="2">
+              <Text
+                pl="5px"
+                pb="8px"
+                color="#605555"
+                fontWeight="medium"
+                fontSize="lg"
+              >
+                Nova senha
+              </Text>
+              <Input
+                size="lg"
+                fontSize="lg"
+                placeholder="Nova senha"
+                readOnly
+              />
+            </Box>
+            <Box mb="20px" flex="1" pl="2">
+              <Text
+                pl="5px"
+                pb="8px"
+                color="#605555"
+                fontWeight="medium"
+                fontSize="lg"
+              >
+                Confirmar senha
+              </Text>
+              <Input
+                size="lg"
+                fontSize="lg"
+                placeholder="Confirmar senha"
+                readOnly
+              />
+            </Box>
+          </Flex>
+          <Flex justify="space-between">
+            <Button
+              marginTop="20px"
+              mb="120px"
+              type="submit"
+              paddingX="24"
+              width="20px"
+              color="white"
+              bg="black"
+              onClick={() => window.history.back()}
+            >
+              Voltar
+            </Button>
+
+            <Button
+              marginTop="30px"
+              mb="120px"
+              type="submit"
+              paddingX="24"
+              width="20px"
+            >
+              Salvar
+            </Button>
+          </Flex>
+        </Box>
+      </form>
+    </Center>
+  );
+}
