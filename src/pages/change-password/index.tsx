93bee029cba61b1dd15819c2421fd0594fb3853d
Pedro Augusto <pedroizarias@iMac-de-Pedro.local>
2023-06-19 00:20:00 -0300

    #127 feat: Adicionando verificação de senhas iguais
    Co-authored-by: Leofbrgs <leonardo81733@icloud.com>

--- src/pages/change-password/index.tsx
+++ src/pages/change-password/index.tsx
@@ -14,113 +14,141 @@
  */
 import { Box, Button, Center, Divider, Flex, Input, Text } from '@chakra-ui/react';
 import { useAuth } from '@/contexts/AuthContext';
+import { useState } from 'react';
+import { toast } from '@/utils/toast';
 
 export function ChangePassword() {
   const { signOut, user } = useAuth();
-
-  return (
-    <form  aria-label="form">
-      <Center
-        aria-label="form"
-        bgGradient="linear(288.94deg, #F8B86D 0%, #F49320 90.96%)"
-        h="100vh"
-        color="white"
-      >
-        <Box
-          position="fixed"
-          left="0"
-          top="0"
-          bottom="0"
-          padding="20px"
-        >
-          <Text fontSize="2.8vw" fontWeight="bold" >
-            Alectrion
-          </Text>
-        </Box>
-        <Box
-          bg="white"
-          borderRadius="10px"
-          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25),  0px 4px 4px rgba(0, 0, 0, 0.25),  0px 1px 1px rgba(0, 0, 0, 0.12),  0px 2px 2px rgba(0, 0, 0, 0.12),  0px 8px 8px rgba(0, 0, 0, 0.12);"
-          color="black"
-          paddingY="20"
-          paddingX="20"
-        >
-          <Text mb="39px" color="#605555" fontWeight="semibold" fontSize="4xl">
-          Alterar senha
-          </Text>
-          <Flex>
-            <Box flex="1" pr="2">
-              <Text
-                pl="5px"
-                pb="8px"
-                color="#605555"
-                fontWeight="medium"
-                fontSize="lg"
-              >
-                Senha atual
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+    
+  const atualizarSenha = () => {
+    if (newPassword !== confirmPassword) {
+      toast.error('As senhas informadas não coincidem. Por favor, verifique e tente novamente.');
+    } else {
+      // Lógica para atualizar a senha
+    }
+  };
+      return (
+        <form aria-label="form">
+          <Center
+            aria-label="form"
+            bgGradient="linear(288.94deg, #F8B86D 0%, #F49320 90.96%)"
+            h="100vh"
+            color="white"
+          >
+            <Box position="fixed" left="0" top="0" bottom="0" padding="20px">
+              <Text fontSize="2.8vw" fontWeight="bold">
+                Alectrion
               </Text>
-              <Input size="lg" fontSize="lg" name='actualPassword' width='50%'/>
             </Box>
-          </Flex>
-          <Flex>
-          <Box>
-              <Text
-                pl="5px"
-                pb="8px"
-                color="#605555"
-                fontWeight="medium"
-                fontSize="lg"
-                marginTop="8px"
-              >
-                Nova senha
+            <Box
+              bg="white"
+              borderRadius="10px"
+              boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25),  0px 4px 4px rgba(0, 0, 0, 0.25),  0px 1px 1px rgba(0, 0, 0, 0.12),  0px 2px 2px rgba(0, 0, 0, 0.12),  0px 8px 8px rgba(0, 0, 0, 0.12);"
+              color="black"
+              paddingY="20"
+              paddingX="20"
+            >
+              <Text mb="39px" color="#605555" fontWeight="semibold" fontSize="4xl">
+                Alterar senha
               </Text>
-              <Input size="lg" fontSize="lg" name='newPassword' width='100%'/>
-          </Box>
-          <Box marginLeft="30px">
+              <Flex>
+                <Box flex="1" pr="2">
+                  <Text
+                    pl="5px"
+                    pb="8px"
+                    color="#605555"
+                    fontWeight="medium"
+                    fontSize="lg"
+                  >
+                    Senha atual
+                  </Text>
+                  <Input size="lg" fontSize="lg" name="actualPassword" width="50%" />
+                </Box>
+              </Flex>
+              <Flex>
+                <Box>
+                  <Text
+                    pl="5px"
+                    pb="8px"
+                    color="#605555"
+                    fontWeight="medium"
+                    fontSize="lg"
+                    marginTop="8px"
+                  >
+                    Nova senha
+                  </Text>
+                  <Input
+                    size="lg"
+                    fontSize="lg"
+                    name="newPassword"
+                    width="100%"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </Box>
+                <Box marginLeft="30px">
+                  <Text
+                    pl="5px"
+                    pb="8px"
+                    color="#605555"
+                    fontWeight="medium"
+                    fontSize="lg"
+                    marginTop="8px"
+                  >
+                    Confirmar senha
+                  </Text>
+                  <Input
+                    size="lg"
+                    fontSize="lg"
+                    name="confirmPassword"
+                    width="100%"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </Box>
+              </Flex>
               <Text
-                pl="5px"
-                pb="8px"
+                mb="5%"
                 color="#605555"
-                fontWeight="medium"
-                fontSize="lg"
-                marginTop="8px"
+                fontWeight="semibold"
+                fontSize="0.7em"
+                marginTop="2%"
               >
-                Confirmar senha
+                A senha deve conter letra maiúscula,
+                <br />
+                letra minúscula, número e caractere
+                <br />
+                especial(@, #, $, %, ...)
               </Text>
-              <Input size="lg" fontSize="lg" name='confirmPassword' width='100%'/>
+              <Flex justify="space-between">
+                <Button
+                  marginTop="30px"
+                  mb="120px"
+                  paddingX="24"
+                  width="20px"
+                  color="white"
+                  bg="black"
+                  onClick={() => window.history.back()}
+                >
+                  Voltar
+                </Button>
+                <Button
+                  marginTop="30px"
+                  mb="120px"
+                  paddingX="24"
+                  width="20px"
+                  color="white"
+                  bg="black"
+                  onClick={atualizarSenha}
+                >
+                  Alterar senha
+                </Button>
+              </Flex>
             </Box>
-          </Flex>     
-            <Text mb="5%" color="#605555" fontWeight="semibold" fontSize="0.7em" marginTop="2%">
-          A senha deve conter letra maiúscula,<br />
-          letra minúscula, número e caractere<br />
-          epecial(@, #, $, %, ...)
-          </Text> 
-          <Flex justify="space-between">
-            <Button
-              marginTop="30px"
-              mb="120px"
-              paddingX="24"
-              width="20px"
-              color="white"
-              bg="black"
-              onClick={() => window.history.back()}
-            >
-              Voltar
-            </Button>
-            <Button
-              marginTop="30px"
-              mb="120px"
-              paddingX="24"
-              width="20px"
-              color="white"
-              bg="black"
-              // onClick={atualizarSenha}
-            >
-              Alterar senha
-            </Button>
-          </Flex>
-        </Box>
-      </Center>
-    </form>
-  );
-}
+          </Center>
+        </form>
+      );
+    }
+    
