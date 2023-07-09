0bcab4954fa1bbfedd55470f44618a8885445c73
João Pedro Chaves <joaopedroaschaves@gmail.com>
2023-07-09 19:21:01 -0300

    fix: adição de verificaçao de token expirado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -16,6 +16,7 @@ interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
   user: SignedUser | null;
+  verifyExpiredToken(): void;
 }
 
 const AuthContext = createContext({} as AuthContextData);
@@ -99,14 +100,48 @@ export function AuthProvider({ children }: AuthProviderProps) {
     navigate('/login');
   }, [navigate]);
 
+  const decodeToken = (token: string): any => {
+    try {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const decodedToken = JSON.parse(window.atob(base64));
+      return decodedToken;
+    } catch (error) {
+      return null; // Token inválido ou erro ao decodificar
+    }
+  };
+
+  const isTokenExpired = useCallback((token: string): boolean => {
+    const decodedToken = decodeToken(token);
+
+    if (!decodedToken || !decodedToken.exp) {
+      return true; // Token inválido ou sem data de expiração
+    }
+
+    const expirationTime = decodedToken.exp * 1000; // Converter para milissegundos
+    const currentTime = Date.now();
+
+    return currentTime >= expirationTime;
+  }, []);
+
+  const verifyExpiredToken = useCallback(() => {
+    const token = String(localStorage.getItem('@alectrion:token'));
+    const expired = isTokenExpired(token);
+    if (expired) {
+      toast.error('Token expirado, faça o login novamente', 'Erro');
+      signOut();
+    }
+  }, [isTokenExpired, signOut]);
+
   const value = useMemo(
     () => ({
       signIn,
       signOut,
       isAuthenticated,
       user,
+      verifyExpiredToken,
     }),
-    [signIn, signOut, isAuthenticated, user]
+    [signIn, signOut, isAuthenticated, user, verifyExpiredToken]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
